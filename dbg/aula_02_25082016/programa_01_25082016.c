378a09bfbeebe796dd39bda46a30906754ac2dac
Daniel Gomes <dbg@a.recife.ifpe.edu.br>
2016-08-25 21:35:30 -0300

    Adicionado o programa pedido na aula_02.

---
+++ dbg/aula_02_25082016/programa_01_25082016.c
@@ -0,0 +1,13 @@
+#include "rims.h"
+
+/* 
+   Programa resposta do exercicio proposto na aula_02 ministrada no
+   dia 25/08/2016.
+*/
+
+void main()
+{
+   while (1) { 
+      B0 = (A0 && A1) || (A0 && A2) || (A1 && A2);
+   }
+}
