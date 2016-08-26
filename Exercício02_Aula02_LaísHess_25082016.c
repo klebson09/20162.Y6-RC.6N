aff342a195a5e5f5cf9043409f978a20697938da
Laís Hess <laishess.adsifpe@gmail.com>
2016-08-25 23:41:56 -0300

    Add files via upload

---
+++ Exercício02_Aula02_LaísHess_25082016.c
@@ -0,0 +1,12 @@
+#include "rims.h"
+
+/* Laís Hess de Oliveira - 20111Y6-RC0330
+   Sistemas Embarcados - Anderson Moreira 
+   Exercício 02 - Aula 02 */
+
+int main(){
+   while (1) { 
+      B0 = (A0 && A1) || (A0 && A2) || (A1 && A2); 
+   }
+   return 0;
+}
